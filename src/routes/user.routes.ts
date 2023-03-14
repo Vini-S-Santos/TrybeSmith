8a271d264b8ac87964451ab68e9ef88a8e985cde
Vinicius <visouza0@gmail.com>
2023-03-14 18:55:27 -0300

    Adiciona requisito 7

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 import UserController from '../controller/user.controller';
+import validationUser from '../middlewares/validationUser';
 
 const router = Router();
 
 const userController = new UserController();
 
-router.post('/', userController.create);
+router.post(
+  '/',
+  validationUser.vocationValidation,
+  validationUser.levelValidation,
+  validationUser.passwordValidation,
+  validationUser.usernameValidation,
+  userController.create,
+);
 
 export default router;
