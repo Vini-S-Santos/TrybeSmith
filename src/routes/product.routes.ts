1a2b1e127a3aecbb8b39fb55323987cdf9529097
Vinicius <visouza0@gmail.com>
2023-03-14 18:24:09 -0300

    Adiciona requisito 6

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import ProductController from '../controller/product.controller';
+import validationProduct from '../middlewares/validationProduct';
 
 const router = Router();
 const productController = new ProductController();
 
-router.post('/', productController.create);
+router.post(
+  '/',
+  validationProduct.productAmountValidation,
+  validationProduct.productNameValidation,
+  productController.create,
+);
 router.get('/', productController.getAll);
 
 export default router;
